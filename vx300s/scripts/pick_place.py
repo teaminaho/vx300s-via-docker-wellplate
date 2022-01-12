cbacd5473fbd50284403b138e073801f3c331288
ykarako <karako.yukihisa@inaho.co>
2022-01-12 13:14:44 +0900

    add over effort stopping

--- vx300s/scripts/pick_place.py
+++ vx300s/scripts/pick_place.py
@@ -1,6 +1,9 @@
-# import time
+import time
+import numpy as np
+import rospy
+from sensor_msgs.msg import JointState
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
-# from interbotix_perception_modules.armtag import InterbotixArmTagInterface
+from interbotix_perception_modules.armtag import InterbotixArmTagInterface
 from interbotix_perception_modules.pointcloud import InterbotixPointCloudInterface
 
 # This script uses a color/depth camera to get the arm to find objects and pick them up.
@@ -12,39 +15,56 @@ from interbotix_perception_modules.pointcloud import InterbotixPointCloudInterfa
 # Then change to this directory and type 'python pick_place.py'
 
 
-def main():
-    # Initialize the arm module along with the pointcloud and armtag modules
-    bot = InterbotixManipulatorXS("vx300s", moving_time=1.5, accel_time=0.75)
-    pcl = InterbotixPointCloudInterface()
-    # armtag = InterbotixArmTagInterface()
+class PickPlaceRunner:
+    MAX_EFFORT = [500.0, 1000.0, 1000.0, 500.0, 500.0, 500.0, 1000.0, 1000.0, 1000.0]
 
-    # set initial arm and gripper pose
-    bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-    bot.gripper.open()
+    def __init__(self, use_calibration=False):
+        rospy.Subscriber("/vx300s/joint_states", JointState, self.callback_states)
 
-    # get the ArmTag pose
-    # bot.arm.set_ee_pose_components(y=-0.3, z=0.2)
-    # time.sleep(0.5)
-    # armtag.find_ref_to_arm_base_transform()
-    # bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+        # Initialize the arm module along with the pointcloud and armtag modules
+        self.bot = InterbotixManipulatorXS("vx300s", moving_time=1.5, accel_time=0.75)
+        self.pcl = InterbotixPointCloudInterface()
+        self.use_calibration = use_calibration
 
-    # get the cluster positions
-    # sort them from max to min 'x' position w.r.t. the 'vx300s/base_link' frame
-    success, clusters = pcl.get_cluster_positions(ref_frame="vx300s/base_link", sort_axis="x", reverse=True)
-    print(f"success: {success}, N: {len(clusters)}")
+    def callback_states(self, joint_states):
+        if np.any(np.abs(joint_states.effort) > self.MAX_EFFORT):
+            rospy.logwarn("Over effort!")
+            rospy.logwarn(f"Current effort values: {joint_states.effort}")
+            rospy.logwarn(f"Maximum effort values: {self.MAX_EFFORT}")
+            rospy.signal_shutdown("Over effort")
+            rospy.spin()
 
-    # pick up all the objects and drop them in a virtual basket in front of the robot
-    for i, cluster in enumerate(clusters):
-        print(f"Target: {i}")
-        x, y, z = cluster["position"]
-        bot.arm.set_ee_pose_components(x=x, y=y, z=z + 0.05, pitch=0.5)
-        bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
-        bot.gripper.close()
-        bot.arm.set_ee_pose_components(x=x, y=y, z=z + 0.05, pitch=0.5)
-        bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-        bot.gripper.open()
-    bot.arm.go_to_sleep_pose()
+    def run(self):
+        # set initial arm and gripper pose
+        self.bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+        self.bot.gripper.open()
+
+        # get the ArmTag pose
+        if self.use_calibration:
+            armtag = InterbotixArmTagInterface()
+            self.bot.arm.set_ee_pose_components(y=-0.3, z=0.2)
+            time.sleep(0.5)
+            armtag.find_ref_to_arm_base_transform()
+            self.bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+
+        # get the cluster positions
+        # sort them from max to min 'x' position w.r.t. the 'vx300s/base_link' frame
+        success, clusters = self.pcl.get_cluster_positions(ref_frame="vx300s/base_link", sort_axis="x", reverse=True)
+        print(f"success: {success}, N: {len(clusters)}")
+
+        # pick up all the objects and drop them in a virtual basket in front of the robot
+        for i, cluster in enumerate(clusters):
+            print(f"Target: {i}")
+            x, y, z = cluster["position"]
+            self.bot.arm.set_ee_pose_components(x=x, y=y, z=z + 0.05, pitch=0.5)
+            self.bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
+            self.bot.gripper.close()
+            self.bot.arm.set_ee_pose_components(x=x, y=y, z=z + 0.05, pitch=0.5)
+            self.bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+            self.bot.gripper.open()
+        self.bot.arm.go_to_sleep_pose()
 
 
 if __name__ == '__main__':
-    main()
+    runner = PickPlaceRunner()
+    runner.run()
--- vx300s/scripts/test_motion.py
+++ vx300s/scripts/test_motion.py
@@ -1,5 +1,7 @@
-from interbotix_xs_modules.arm import InterbotixManipulatorXS
 import numpy as np
+import rospy
+from sensor_msgs.msg import JointState
+from interbotix_xs_modules.arm import InterbotixManipulatorXS
 
 # This script makes the end-effector perform pick, pour, and place tasks
 #
@@ -7,24 +9,39 @@ import numpy as np
 # Then change to this directory and type 'python bartender.py'
 
 
-def main():
-    bot = InterbotixManipulatorXS("vx300s", "arm", "gripper")
-    bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-    bot.arm.set_single_joint_position("waist", np.pi / 2.0)
-    bot.gripper.open()
-    bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
-    bot.gripper.close()
-    bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
-    bot.arm.set_single_joint_position("waist", -np.pi / 2.0)
-    bot.arm.set_ee_cartesian_trajectory(pitch=1.5)
-    bot.arm.set_ee_cartesian_trajectory(pitch=-1.5)
-    bot.arm.set_single_joint_position("waist", np.pi / 2.0)
-    bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
-    bot.gripper.open()
-    bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
-    bot.arm.go_to_home_pose()
-    bot.arm.go_to_sleep_pose()
+class TestMotionRunner:
+    MAX_EFFORT = [500.0, 1000.0, 1000.0, 500.0, 500.0, 500.0, 1000.0, 1000.0, 1000.0]
+
+    def __init__(self):
+        rospy.Subscriber("/vx300s/joint_states", JointState, self.callback_states)
+        self.bot = InterbotixManipulatorXS("vx300s", "arm", "gripper")
+
+    def callback_states(self, joint_states):
+        if np.any(np.abs(joint_states.effort) > self.MAX_EFFORT):
+            rospy.logwarn("Over effort!")
+            rospy.logwarn(f"Current effort values: {joint_states.effort}")
+            rospy.logwarn(f"Maximum effort values: {self.MAX_EFFORT}")
+            rospy.signal_shutdown("Over effort")
+            rospy.spin()
+
+    def run(self):
+        self.bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+        self.bot.arm.set_single_joint_position("waist", np.pi / 2.0)
+        self.bot.gripper.open()
+        self.bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
+        self.bot.gripper.close()
+        self.bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
+        self.bot.arm.set_single_joint_position("waist", -np.pi / 2.0)
+        self.bot.arm.set_ee_cartesian_trajectory(pitch=1.5)
+        self.bot.arm.set_ee_cartesian_trajectory(pitch=-1.5)
+        self.bot.arm.set_single_joint_position("waist", np.pi / 2.0)
+        self.bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
+        self.bot.gripper.open()
+        self.bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
+        self.bot.arm.go_to_home_pose()
+        self.bot.arm.go_to_sleep_pose()
 
 
 if __name__ == '__main__':
-    main()
+    runner = TestMotionRunner()
+    runner.run()
