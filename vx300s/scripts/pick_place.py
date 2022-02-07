f0e1dd33da4e176a79e3cc1d2a56927141acc1b4
ykarako <karako.yukihisa@inaho.co>
2022-02-08 02:19:52 +0900

    tune scripts

--- vx300s/scripts/pick_place.py
+++ vx300s/scripts/pick_place.py
@@ -1,9 +1,7 @@
-import time
 import numpy as np
 import rospy
 from sensor_msgs.msg import JointState
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
-from interbotix_perception_modules.armtag import InterbotixArmTagInterface
 from interbotix_perception_modules.pointcloud import InterbotixPointCloudInterface
 
 # This script uses a color/depth camera to get the arm to find objects and pick them up.
@@ -16,15 +14,14 @@ from interbotix_perception_modules.pointcloud import InterbotixPointCloudInterfa
 
 
 class PickPlaceRunner:
-    MAX_EFFORT = [500.0, 1000.0, 1000.0, 500.0, 500.0, 500.0, 1000.0, 1000.0, 1000.0]
+    MAX_EFFORT = [1200.0, 1500.0, 1000.0, 500.0, 500.0, 500.0, 1000.0, 1000.0, 1000.0]
 
-    def __init__(self, use_calibration=False):
+    def __init__(self):
         rospy.Subscriber("/vx300s/joint_states", JointState, self.callback_states)
 
         # Initialize the arm module along with the pointcloud and armtag modules
         self.bot = InterbotixManipulatorXS("vx300s", moving_time=1.5, accel_time=0.75)
         self.pcl = InterbotixPointCloudInterface()
-        self.use_calibration = use_calibration
 
     def callback_states(self, joint_states):
         if np.any(np.abs(joint_states.effort) > self.MAX_EFFORT):
@@ -35,34 +32,40 @@ class PickPlaceRunner:
             rospy.spin()
 
     def run(self):
-        # set initial arm and gripper pose
-        self.bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-        self.bot.gripper.open()
-
-        # get the ArmTag pose
-        if self.use_calibration:
-            armtag = InterbotixArmTagInterface()
-            self.bot.arm.set_ee_pose_components(y=-0.3, z=0.2)
-            time.sleep(0.5)
-            armtag.find_ref_to_arm_base_transform()
-            self.bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-
-        # get the cluster positions
-        # sort them from max to min 'x' position w.r.t. the 'vx300s/base_link' frame
-        success, clusters = self.pcl.get_cluster_positions(ref_frame="vx300s/base_link", sort_axis="x", reverse=True)
-        print(f"success: {success}, N: {len(clusters)}")
-
-        # pick up all the objects and drop them in a virtual basket in front of the robot
-        for i, cluster in enumerate(clusters):
-            print(f"Target: {i}")
-            x, y, z = cluster["position"]
-            self.bot.arm.set_ee_pose_components(x=x, y=y, z=z + 0.05, pitch=0.5)
-            self.bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
-            self.bot.gripper.close()
-            self.bot.arm.set_ee_pose_components(x=x, y=y, z=z + 0.05, pitch=0.5)
-            self.bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+        while True:
+            # set initial arm and gripper pose
+            self.bot.arm.set_ee_pose_components(x=0.16, z=0.2, pitch=0.5)
             self.bot.gripper.open()
-        self.bot.arm.go_to_sleep_pose()
+
+            # get the cluster positions
+            # sort them from max to min 'x' position w.r.t. the 'vx300s/base_link' frame
+            success, clusters = self.pcl.get_cluster_positions(ref_frame="vx300s/base_link", sort_axis="x", reverse=True)
+            print(f"success: {success}, N: {len(clusters)}")
+
+            # pick up all the objects and drop them in a virtual basket in front of the robot
+            for i, cluster in enumerate(clusters):
+                print(f"Target: {i}")
+                x, y, z = cluster["position"]
+
+                # picking
+                self.bot.arm.set_ee_pose_components(x=x, y=y, z=z + 0.06, pitch=0.5)
+                self.bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
+                self.bot.gripper.close()
+
+                # placing
+                self.bot.arm.set_ee_pose_components(x=x, y=y, z=z + 0.06, pitch=0.5)
+                self.bot.arm.set_ee_pose_components(x=0.1, y=0.25, z=z + 0.06, pitch=0.5)
+                self.bot.arm.set_ee_pose_components(x=0.1, y=0.25, z=z, pitch=0.5)
+                self.bot.gripper.open()
+                self.bot.arm.set_ee_pose_components(x=0.1, y=0.25, z=z + 0.06, pitch=0.5)
+
+            # back to the initial arm and gripper pose
+            self.bot.arm.set_ee_pose_components(x=0.16, z=0.2, pitch=0.5)
+
+            key = input("Enter 'q' key to exit, enter other keys to continue.\n")
+            if key == 'q':
+                self.bot.arm.go_to_sleep_pose()
+                break
 
 
 if __name__ == '__main__':
