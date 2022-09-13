f8160d6b673f1e29d975a841db967fc6d9f4b2e6
Takehiro Matsushima <takehiro.dreamizm@gmail.com>
2022-09-13 18:03:29 +0900

    Add `docker-entrypoint`s

--- vx300s/docker-entrypoint.sh
+++ vx300s/docker-entrypoint.sh
@@ -3,8 +3,4 @@
 # Setup catkin workspace
 . ~/interbotix_ws/devel/setup.bash
 
-# Start ros nodes
-# roslaunch --no-summary --screen --wait interbotix_xsarm_descriptions xsarm_description.launch robot_model:=vx300s use_joint_pub_gui:=true
-# roslaunch --no-summary --screen --wait interbotix_xsarm_control xsarm_control.launch robot_model:=vx300s $@
-roslaunch --no-summary --screen --wait arm_ctrl arm_ctrl.launch robot_model:=vx300s $@
-# roslaunch --no-summary --screen --wait interbotix_xsarm_perception xsarm_perception.launch robot_model:=vx300s use_pointcloud_tuner_gui:=true use_armtag_tuner_gui:=true
+/bin/bash -c "$@"
